af66ed08ceab9516d249518c6474b6a58f392d0e
joakimolofsson <jjoakim.olofsson@gmail.com>
2026-03-07 15:47:14 +0100

    feat: add gun effects

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,15 +7,15 @@ const FRICTION = 10
 
 
 func _physics_process(delta: float) -> void:
-	var arm_dir = get_global_mouse_position() - arm.global_position
+	#var arm_dir = get_global_mouse_position() - arm.global_position
 	arm.look_at(get_global_mouse_position())
 	arm.rotation += deg_to_rad(-90)
-	arm.flip_h = arm_dir.x > 0
-	
-	if arm.flip_h:
-		arm.position.y = -18
-	else:
-		arm.position.y = -16
+	#arm.flip_h = arm_dir.x > 0
+	#
+	#if arm.flip_h:
+		#arm.position.y = -18
+	#else:
+		#arm.position.y = -16
 	
 	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 	if direction:
