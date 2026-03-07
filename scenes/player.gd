845a0e2006d125a28d50d6603881844c99f5249e
joakimolofsson <jjoakim.olofsson@gmail.com>
2026-03-07 14:14:50 +0100

    feat: add arm rotation

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,25 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const FRICTION = 10
+@onready var arm = $SpriteArm
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var arm_dir = get_global_mouse_position() - arm.global_position
+	arm.look_at(get_global_mouse_position())
+	arm.rotation += deg_to_rad(-90)
+	arm.flip_h = arm_dir.x > 0
+	
+	if arm.flip_h:
+		arm.position.y = -18
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		arm.position.y = -16
+	
+	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	if direction:
+		velocity = direction * SPEED
+		print(velocity)
+	else:
+		#velocity = move_toward(velocity, 0, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
 	move_and_slide()
